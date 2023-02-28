94adc599bda9dd6cbf4d005437b3cab15add05b9
guancongxin <915157930@qq.com>
2023-02-28 22:51:55 +0800

    bazel gtest demo

---
+++ cpp-demo/gtest/lib/greet.cc
@@ -0,0 +1,6 @@
+#include "greet.h"
+
+int Greet::hello(std::string name){
+    std::cout << "hello " << name << std::endl;
+    return 0;
+}
---
+++ cpp-demo/gtest/lib/greet.h
@@ -0,0 +1,11 @@
+#ifndef _GREET_H_
+#define _GREET_H_
+#include <iostream>
+
+class Greet{
+    public:
+    static int hello(std::string name);
+};
+
+
+#endif // _GREET_H_
